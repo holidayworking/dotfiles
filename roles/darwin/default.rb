4e86f7e0e3167ebeb2f298a51366f134b446b576
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2020-01-29 08:07:30 +0900

    Add vagrant-notify-forwarder

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -18,6 +18,6 @@ execute "cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf #{ENV['HOME']}/Librar
   not_if "test -f #{ENV['HOME']}/Library/Fonts/Ricty-Bold.ttf"
 end
 
-%w[sahara vagrant-hostsupdater vagrant-vbguest].each do |plugin|
+%w[sahara vagrant-hostsupdater vagrant-notify-forwarder vagrant-vbguest].each do |plugin|
   vagrant_plugin plugin
 end
