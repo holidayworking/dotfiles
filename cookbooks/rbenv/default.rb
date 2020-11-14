2586d26b493fed335a578af26da543c41141cbde
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2020-11-15 07:41:56 +0900

    Update Ruby 2.7.2

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -1,11 +1,12 @@
 node.reverse_merge!(
   rbenv: {
     user: node[:user],
-    global: '2.6.6',
+    global: '2.7.2',
     versions: %w[
       2.4.4
       2.5.0
       2.6.6
+      2.7.2
     ]
   }
 )
