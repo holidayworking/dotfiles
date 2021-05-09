635cd64a06da1e53bd5e76659ee5c3f0d248e28e
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2021-05-09 16:18:09 +0900

    Change fish exec from ~/.bashrc to ~/.bash_profile

--- cookbooks/fish/default.rb
+++ cookbooks/fish/default.rb
@@ -1,8 +1,8 @@
 dotfile '.config/fish'
 
 if node[:os] == 'linux'
-  execute "echo 'exec /home/linuxbrew/.linuxbrew/bin/fish' >> #{ENV['HOME']}/.bashrc" do
+  execute "echo 'exec /home/linuxbrew/.linuxbrew/bin/fish' >> #{ENV['HOME']}/.bash_profile" do
     user node[:user]
-    not_if "grep '^exec /home/linuxbrew/.linuxbrew/bin/fish' #{ENV['HOME']}/.bashrc"
+    not_if "grep '^exec /home/linuxbrew/.linuxbrew/bin/fish' #{ENV['HOME']}/.bash_profile"
   end
 end
