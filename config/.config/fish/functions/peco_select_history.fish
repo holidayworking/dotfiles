77328143f06b697e43bd17c35a6e6e04b51c8629
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2021-05-10 20:10:44 +0900

    Add oh-my-fish/plugin-peco

--- config/.config/fish/functions/peco_select_history.fish
+++ config/.config/fish/functions/peco_select_history.fish
@@ -1,14 +1,14 @@
 function peco_select_history
   if test (count $argv) = 0
-    set peco_flags
+    set peco_flags --layout=bottom-up
   else
-    set peco_flags --query "$argv"
+    set peco_flags --layout=bottom-up --query "$argv"
   end
 
-  history | peco $peco_flags | read command
+  history|peco $peco_flags|read foo
 
-  if [ $command ]
-    commandline $command
+  if [ $foo ]
+    commandline $foo
   else
     commandline ''
   end
