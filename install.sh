5fa16811ad1b6fc88df12c28816c84d09d8fd252
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2022-01-02 11:49:19 +0900

    Remove log level

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ bin/setup
 # Homebrew does not allow sudo.
 case "$(uname)" in
   "Darwin")  bin/mitamae local "$@" lib/recipe.rb ;;
-  *) sudo -E bin/mitamae local "$@" --log-level=debug lib/recipe.rb ;;
+  *) sudo -E bin/mitamae local "$@" lib/recipe.rb ;;
 esac
