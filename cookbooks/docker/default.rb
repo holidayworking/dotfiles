766d90c11b952388a8c6caf1f024d87cd25a57b8
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2022-02-19 10:14:18 +0900

    Docker Rootless mode

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -4,26 +4,45 @@ node.reverse_merge!(
   }
 )
 
-execute 'curl -sSL https://get.docker.com/ | sh' do
+execute 'curl -fsSL https://get.docker.com/ | sh' do
   not_if 'which docker'
 end
 
-service 'docker' do
-  action [:enable, :start]
+%w[
+  docker.service
+  docker.socket
+].each do |svc|
+  service svc do
+    action [:disable, :stop]
+  end
 end
 
-execute "usermod -aG docker #{node[:user]}" do
-  not_if "groups #{node[:user]} | grep docker -w"
-end
+package 'uidmap'
 
-execute 'mkdir -p /usr/local/lib/docker/cli-plugins' do
-  not_if 'test -d /usr/local/lib/docker/cli-plugins'
-end
-
-execute 'install docker-compose' do
+execute "install Docker Rootless mode" do
   command <<-CMD
-    curl -SL https://github.com/docker/compose/releases/download/v#{node[:docker_compose][:version]}/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose \
-    && chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+    export XDG_RUNTIME_DIR=/run/user/$(id -u #{node[:user]})
+    dockerd-rootless-setuptool.sh install
   CMD
+  user node[:user]
+  not_if "test -d #{ENV['HOME']}/.docker"
+end
+
+execute 'docker context use rootless' do
+  user node[:user]
+  not_if 'docker context ls | grep "rootless \*"'
+end
+
+execute "mkdir #{ENV['HOME']}/.docker/cli-plugins" do
+  user node[:user]
+  not_if "test -d #{ENV['HOME']}/.docker/cli-plugins"
+end
+
+execute 'install Docker Compose' do
+  command <<-CMD
+    curl -o #{ENV['HOME']}/.docker/cli-plugins/docker-compose -fsSL https://github.com/docker/compose/releases/download/v#{node[:docker_compose][:version]}/docker-compose-linux-x86_64
+    chmod +x #{ENV['HOME']}/.docker/cli-plugins/docker-compose
+  CMD
+  user node[:user]
   not_if "docker compose version | grep #{node[:docker_compose][:version]}"
 end
