3b86a198569582666aaaf7d9b15273e55af488c6
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2022-08-13 17:54:13 +0900

    Remove manual install Docker Compose

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,9 +1,3 @@
-node.reverse_merge!(
-  docker_compose: {
-    version: '2.2.3'
-  }
-)
-
 execute 'curl -fsSL https://get.docker.com/ | sh' do
   not_if 'which docker'
 end
@@ -19,7 +13,7 @@ end
 
 package 'uidmap'
 
-execute "install Docker Rootless mode" do
+execute 'install Docker Rootless mode' do
   command <<-CMD
     export XDG_RUNTIME_DIR=/run/user/$(id -u #{node[:user]})
     dockerd-rootless-setuptool.sh install
@@ -31,18 +25,4 @@ end
 execute 'docker context use rootless' do
   user node[:user]
   not_if 'docker context ls | grep "rootless \*"'
-end
-
-execute "mkdir #{ENV['HOME']}/.docker/cli-plugins" do
-  user node[:user]
-  not_if "test -d #{ENV['HOME']}/.docker/cli-plugins"
-end
-
-execute 'install Docker Compose' do
-  command <<-CMD
-    curl -o #{ENV['HOME']}/.docker/cli-plugins/docker-compose -fsSL https://github.com/docker/compose/releases/download/v#{node[:docker_compose][:version]}/docker-compose-linux-x86_64
-    chmod +x #{ENV['HOME']}/.docker/cli-plugins/docker-compose
-  CMD
-  user node[:user]
-  not_if "docker compose version | grep #{node[:docker_compose][:version]}"
 end
