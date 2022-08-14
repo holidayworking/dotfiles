bfee1bcdbd6b54415a132e661c4283887140867f
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2022-08-15 08:03:26 +0900

    Fix check path

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -2,7 +2,7 @@ brew_dir = node['kernel']['machine'] == 'arm64' ? '/opt/homebrew' : '/usr/local'
 
 execute 'install homebrew' do
   command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
-  not_if "which #{brew_dir}/bin/path"
+  not_if "which #{brew_dir}/bin/brew"
 end
 
 dotfile '.Brewfile' do
