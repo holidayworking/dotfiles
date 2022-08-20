61000b7246ac8b777132302ba415899842c5d72f
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2022-08-20 23:28:20 +0900

    Remove brew shellenv

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-brew_dir = node['kernel']['machine'] == 'arm64' ? '/opt/homebrew' : '/usr/local'
-
 execute 'install homebrew' do
   command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
-  not_if "which #{brew_dir}/bin/brew"
+  not_if 'which brew'
 end
 
 dotfile '.Brewfile' do
   source '.Brewfile.darwin'
 end
 
-execute "eval $(#{brew_dir}/bin/brew shellenv) && brew bundle install --global" do
-  not_if "eval $(#{brew_dir}/bin/brew shellenv) && brew bundle check --global"
+execute 'brew bundle install --global' do
+  not_if 'brew bundle check --global'
 end
