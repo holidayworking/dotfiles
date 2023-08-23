dd3b56a4725c1d736e95e9055d58e9db22dfafde
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2023-08-23 18:56:34 +0900

    chore: remove gnome-shell-extension-search-light-git

--- cookbooks/gnome/default.rb
+++ cookbooks/gnome/default.rb
@@ -2,9 +2,4 @@
 
 package 'dconf-editor'
 
-%w[
-  gnome-shell-extension-pano
-  gnome-shell-extension-search-light-git
-].each do |pkg|
-  aur_package pkg
-end
+aur_package 'gnome-shell-extension-pano'
