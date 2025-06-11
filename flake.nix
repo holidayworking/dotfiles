d4eb076793ee66777545c63cb264ed82f89f437e
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2025-06-11 21:37:34 +0900

    chore: switch nixpkgs input from nixpkgs-25.05-darwin to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
