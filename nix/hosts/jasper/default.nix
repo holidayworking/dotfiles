c6d8579d9e3ba58881fed83393986f50f0fd1945
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2025-06-11 19:55:38 +0900

    refactor: remove darwin module

--- nix/hosts/jasper/default.nix
+++ nix/hosts/jasper/default.nix
@@ -2,20 +2,103 @@
 let
   inherit (inputs) nix-darwin home-manager nixpkgs;
 
-  system = "aarch64-darwin";
   pkgs = import nixpkgs { inherit system; };
 
-  username = "hidekazu";
   configuration =
     { ... }:
     {
+      homebrew = {
+        enable = true;
+        brews = [ "mas" ];
+        casks = [
+          "1password"
+          "appcleaner"
+          "chatgpt"
+          "docker"
+          "font-fira-code"
+          "hyper"
+          "karabiner-elements"
+          "maccy"
+          "microsoft-edge"
+          "notion"
+          "ogdesign-eagle"
+          "visual-studio-code"
+        ];
+        masApps = {
+          "1Password for Safari" = 1569813296;
+          "Eagle for Safari" = 1526651672;
+          "Keynote" = 409183694;
+          "Numbers" = 409203825;
+          "Pages" = 409201541;
+          "Photomator" = 1444636541;
+          "Slack" = 803453959;
+          "Tailscale" = 1475387142;
+          "The Unarchiver" = 425424353;
+          "Toggl Track" = 1291898086;
+          "Windows App" = 1295203466;
+        };
+        onActivation = {
+          autoUpdate = true;
+          cleanup = "zap";
+        };
+      };
+      security = {
+        pam.services.sudo_local = {
+          reattach = true;
+          touchIdAuth = true;
+        };
+      };
       networking.hostName = "jasper";
+      nix = {
+        gc = {
+          automatic = true;
+          interval = {
+            Weekday = 0;
+            Hour = 0;
+            Minute = 0;
+          };
+          options = "--delete-older-than 7d";
+        };
+        optimise.automatic = true;
+        settings = {
+          experimental-features = [
+            "flakes"
+            "nix-command"
+          ];
+        };
+      };
+      nixpkgs = {
+        hostPlatform = system;
+      };
+      system = {
+        defaults = {
+          SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+          dock = {
+            minimize-to-application = false;
+            mru-spaces = false;
+            show-recents = false;
+            wvous-bl-corner = 4;
+            wvous-br-corner = 5;
+            wvous-tl-corner = 2;
+            wvous-tr-corner = 3;
+          };
+          finder = {
+            NewWindowTarget = "Home";
+            ShowExternalHardDrivesOnDesktop = false;
+            ShowRemovableMediaOnDesktop = false;
+          };
+          menuExtraClock.ShowSeconds = true;
+        };
+        primaryUser = username;
+        stateVersion = 5;
+      };
     };
+  system = "aarch64-darwin";
+  username = "hidekazu";
 in
 nix-darwin.lib.darwinSystem {
   modules = [
     configuration
-    ../../modules/darwin
     home-manager.darwinModules.home-manager
     {
       home-manager = {
@@ -31,5 +114,4 @@ nix-darwin.lib.darwinSystem {
       };
     }
   ];
-  specialArgs = { inherit system username; };
 }
--- nix/modules/darwin/security/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  pam.services.sudo_local = {
-    touchIdAuth = true;
-    reattach = true;
-  };
-}
