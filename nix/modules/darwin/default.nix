72b2441650b0f3cc78faa7d7d9584aacc3200b8d
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2025-06-11 19:40:11 +0900

    feat: enable garbage collection

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -9,6 +9,15 @@
   system = import ./system { inherit username; };
 
   nix = {
+    gc = {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
+      options = "--delete-older-than 7d";
+    };
     optimise.automatic = true;
     settings = {
       experimental-features = [
