1f7d56a2f36e36292d2a87b387b4ea16d1e5f03f
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2025-06-23 20:27:32 +0900

    feat(nix): create darwin role

--- nix/machines/macbook-air-m2.nix
+++ nix/machines/macbook-air-m2.nix
@@ -8,105 +8,20 @@ let
 in
 nix-darwin.lib.darwinSystem {
   modules = [
-    home-manager.darwinModules.home-manager
-    ../roles/base
+    ../roles/darwin
     {
-      home-manager = {
-        users."${username}" = {
-          home = {
-            homeDirectory = nixpkgs.lib.mkForce "/Users/${username}/";
-            stateVersion = "25.05";
-          };
-
-          targets.darwin.defaults."com.apple.Safari" = {
-            AutoFillPasswords = false;
-            AutoOpenSafeDownloads = false;
-            IncludeDevelopMenu = true;
-          };
-        };
-      };
-
-      homebrew = {
-        enable = true;
-        brews = [ "mas" ];
-
-        casks = [
-          "1password"
-          "appcleaner"
-          "chatgpt"
-          "docker"
-          "font-fira-code"
-          "hyper"
-          "karabiner-elements"
-          "maccy"
-          "microsoft-edge"
-          "notion"
-          "ogdesign-eagle"
-          "utm"
-          "visual-studio-code"
-        ];
-
-        masApps = {
-          "1Password for Safari" = 1569813296;
-          "Eagle for Safari" = 1526651672;
-          "Keynote" = 409183694;
-          "Numbers" = 409203825;
-          "Pages" = 409201541;
-          "Photomator" = 1444636541;
-          "Slack" = 803453959;
-          "Tailscale" = 1475387142;
-          "The Unarchiver" = 425424353;
-          "Toggl Track" = 1291898086;
-          "Windows App" = 1295203466;
-        };
-
-        onActivation = {
-          autoUpdate = true;
-          cleanup = "zap";
-        };
-      };
-
-      security = {
-        pam.services.sudo_local = {
-          reattach = true;
-          touchIdAuth = true;
-        };
-      };
-
-      networking = {
-        computerName = hostname;
-        hostName = hostname;
-      };
-
-      system = {
-        primaryUser = username;
-        stateVersion = 5;
-
-        defaults = {
-          SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
-          menuExtraClock.ShowSeconds = true;
-
-          dock = {
-            minimize-to-application = true;
-            mru-spaces = false;
-            show-recents = false;
-            wvous-bl-corner = 4;
-            wvous-br-corner = 5;
-            wvous-tl-corner = 2;
-            wvous-tr-corner = 3;
-          };
-
-          finder = {
-            FXRemoveOldTrashItems = true;
-            NewWindowTarget = "Home";
-            ShowExternalHardDrivesOnDesktop = false;
-            ShowRemovableMediaOnDesktop = false;
-          };
-        };
-      };
+      home-manager.users."${username}".home.stateVersion = "25.05";
+      system.stateVersion = 5;
     }
   ];
+
   specialArgs = {
-    inherit nixpkgs system username;
+    inherit
+      nixpkgs
+      home-manager
+      system
+      hostname
+      username
+      ;
   };
 }
