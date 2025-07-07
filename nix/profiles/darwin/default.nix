e58b896a17d568b112958ef48541222a7fd50607
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2025-07-07 22:13:44 +0900

    refactor(nix): separate homebrew configuration from darwin profile

--- nix/profiles/darwin/default.nix
+++ nix/profiles/darwin/default.nix
@@ -10,6 +10,7 @@
   imports = [
     home-manager.darwinModules.home-manager
     ../common
+    ./homebrew.nix
   ];
 
   home-manager = {
@@ -23,47 +24,6 @@
         AutoOpenSafeDownloads = false;
         IncludeDevelopMenu = true;
       };
-    };
-  };
-
-  homebrew = {
-    enable = true;
-    brews = [ "mas" ];
-
-    casks = [
-      "1password"
-      "appcleaner"
-      "chatgpt"
-      "docker-desktop"
-      "font-fira-code"
-      "ghostty"
-      "karabiner-elements"
-      "maccy"
-      "microsoft-edge"
-      "notion"
-      "ogdesign-eagle"
-      "utm"
-      "visual-studio-code"
-    ];
-
-    masApps = {
-      "1Password for Safari" = 1569813296;
-      "Eagle for Safari" = 1526651672;
-      "Keynote" = 409183694;
-      "Numbers" = 409203825;
-      "Pages" = 409201541;
-      "Photomator" = 1444636541;
-      "Slack" = 803453959;
-      "Tailscale" = 1475387142;
-      "The Unarchiver" = 425424353;
-      "Toggl Track" = 1291898086;
-      "Windows App" = 1295203466;
-      "Xiaomi Interconnectivity" = 6673908449;
-    };
-
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
     };
   };
 
