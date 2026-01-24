276b43031a577f1fc09d1503ab483b28d29f2fe6
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-01-24 18:21:14 +0900

    refactor(nix): migrate Ghostty and 1Password to home-manager modules

--- modules/deskstop/utils/1password.nix
+++ modules/deskstop/utils/1password.nix
@@ -1,11 +1,12 @@
 {
-  flake.modules.darwin.desktop = {
-    homebrew = {
-      casks = [
-        "1password"
-      ];
+  flake.modules.darwin.desktop =
+    { pkgs, ... }:
+    {
+      homebrew.masApps."1Password for Safari" = 1569813296;
 
-      masApps."1Password for Safari" = 1569813296;
+      programs._1password-gui = {
+        enable = true;
+        package = pkgs.brewCasks."1password";
+      };
     };
-  };
 }
