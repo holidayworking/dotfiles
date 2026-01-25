43eeef2ead53b7be6f5e49fdf4c5072ec7c4fe5a
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-01-25 10:54:34 +0900

    feat(awscli): add SSO session and profiles configuration

--- modules/dev/awscli.nix
+++ modules/dev/awscli.nix
@@ -2,6 +2,44 @@
   flake.modules.homeManager.dev =
     { pkgs, ... }:
     {
-      programs.awscli.enable = true;
+      programs.awscli = {
+        enable = true;
+
+        settings = {
+          "sso-session default" = {
+            sso_start_url = "https://d-956774a6a5.awsapps.com/start/";
+            sso_region = "ap-northeast-1";
+            sso_registration_scopes = "sso:account:access";
+          };
+
+          "profile main" = {
+            sso_session = "default";
+            sso_account_id = "766612536658";
+            sso_role_name = "AdministratorAccess";
+            region = "ap-northeast-1";
+          };
+
+          "profile master" = {
+            sso_session = "default";
+            sso_account_id = "255571037684";
+            sso_role_name = "AdministratorAccess";
+            region = "ap-northeast-1";
+          };
+
+          "profile sandbox" = {
+            sso_session = "default";
+            sso_account_id = "978053779240";
+            sso_role_name = "AdministratorAccess";
+            region = "ap-northeast-1";
+          };
+
+          "profile security-operation" = {
+            sso_session = "default";
+            sso_account_id = "376129857884";
+            sso_role_name = "AdministratorAccess";
+            region = "ap-northeast-1";
+          };
+        };
+      };
     };
 }
