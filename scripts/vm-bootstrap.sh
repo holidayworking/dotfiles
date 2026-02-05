0d3ef50ddfcfd99ca8db2ea295b291ead32de83b
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-02-05 21:14:39 +0900

    feat: cancer

--- scripts/vm-bootstrap.sh
+++ scripts/vm-bootstrap.sh
@@ -2,14 +2,15 @@
 
 set -euC
 
+readonly FLAKE_URL="${FLAKE_URL:-.#gemini}"
 readonly VM_NAME="${VM_NAME:-NixOS}"
 readonly VM_IP="${VM_IP}"
 
 echo "Bootstrapping NixOS on VM..."
 nix run github:nix-community/nixos-anywhere -- \
-  --flake .#gemini \
-  --target-host root@"$VM_IP" \
-  --phases kexec,disko,install
+	--flake "$FLAKE_URL" \
+	--target-host root@"$VM_IP" \
+	--phases kexec,disko,install
 
 echo "Restarting VM..."
 prlctl stop "$VM_NAME" --kill
