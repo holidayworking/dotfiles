6de50629eed3572e9e0f3fd0b1f625fbc1b8a28f
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-02-06 20:48:59 +0900

    feat: add Codex

---
+++ modules/programs/codex.nix
@@ -0,0 +1,26 @@
+{
+  delib,
+  host,
+  mcpServers,
+  inputs,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.codex";
+
+  options = delib.singleEnableOption host.shellFeatured;
+
+  home.ifEnabled.programs.codex = {
+    enable = true;
+
+    settings.mcp_servers =
+      (inputs.mcp-servers-nix.lib.evalModule pkgs (
+        mcpServers
+        // {
+          format = "toml-inline";
+          flavor = "codex";
+        }
+      )).config.settings.servers;
+  };
+}
