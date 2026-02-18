07beb17282862d73af2f5f9d0106d893e67c29ec
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-02-18 17:43:05 +0900

    feat: add locale

---
+++ modules/toplevel/i18n.nix
@@ -0,0 +1,27 @@
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "i18n";
+
+  options = delib.singleEnableOption host.isDesktop;
+
+  nixos.ifEnabled.i18n = {
+    defaultLocale = "ja_JP.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
+  };
+}
